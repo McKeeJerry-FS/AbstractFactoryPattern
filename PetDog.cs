6304fc7aef2397391ccfb07c8c2cb6eda80fe6e5
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-06-27 19:06:45 -0400

    created the pet classes

---
+++ PetDog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractFactoryPattern.Interfaces;
+
+namespace AbstractFactoryPattern
+{
+    public class PetDog : IDog
+    {
+        public void AboutMe()
+        {
+            Console.WriteLine("Pet Dog says, \"I prefer to stay at home. Bow-wow.\"");
+
+        }
+    }
+}
