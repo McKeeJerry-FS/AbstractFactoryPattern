e9132fb6ac6f2566b68a744524969ca794d14052
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-06-27 18:55:39 -0400

    added the wild classes and interfaces

---
+++ WildDog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractFactoryPattern.Interfaces;
+
+namespace AbstractFactoryPattern
+{
+    public class WildDog : IDog
+    {
+        public void AboutMe()
+        {
+            Console.WriteLine("Wild Dog says, \"I prefer to roam freely in the jungle. Bow-wow.\"");
+        }
+    }
+}
---
+++ WildTiger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractFactoryPattern.Interfaces;
+
+namespace AbstractFactoryPattern
+{
+    public class WildTiger : ITiger
+    {
+        public void AboutMe()
+        {
+            Console.WriteLine("Wild Tiger says, \"I prefer hunting in the jungle. Halum!\"");
+        }
+    }
+}
