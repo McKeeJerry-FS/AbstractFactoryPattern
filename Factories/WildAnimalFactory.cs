b6626acf61b5a40956a4675ac1f2480b0ddea09a
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-06-27 19:21:44 -0400

    added a factory provider

---
+++ Factories/WildAnimalFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractFactoryPattern.Factories;
+using AbstractFactoryPattern.Interfaces;
+
+namespace AbstractFactoryPattern.Factories
+{
+    public class WildAnimalFactory : IAnimalFactory
+    {
+        public IDog GetDog()
+        {
+            return new WildDog();
+        }
+
+        public ITiger GetTiger()
+        {
+            return new WildTiger();
+        }
+    }
+}
